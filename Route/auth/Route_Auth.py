2605334c49881264ab17578c5e0bc94f296d8941
jeived <jeived777@yandex.ru>
2025-10-08 00:25:33 +0300

    Реализована авторизация и регистрация, валидация данных для них

--- Route/auth/Route_Auth.py
+++ Route/auth/Route_Auth.py
@@ -1,45 +1,49 @@
-from urllib.request import Request
-
 from fastapi import APIRouter
-from pydantic import BaseModel
+
+from Route.auth.response_model import CheckRegistationRequestModel , CheckDataResponseModel
 
 router = APIRouter ( )
 
 
-
-
-
-
 @router.get ( "/login" , summary = "Авторизация пользователя" )
-async def login_auth () :
+async def login_auth ( ) :
 	pass
 
 
-@router.get ( "/register" ,  summary = "Регистрация пользователя" )
+@router.get (
+	"/check_data_register" , summary = "Проверка данных для авторизации" ,
+	response_model = CheckDataResponseModel
+	)
+async def check_data_register ( data: CheckRegistationRequestModel ) :
+	return CheckDataResponseModel (
+		success = True ,
+		message = "Все данные валидны. Можно переходить к отправке кода подтверждения."
+		)
+
+
+@router.get ( "/register" , summary = "Регистрация пользователя" )
 async def register_auth ( ) :
 	pass
 
 
-@router.patch (  "/reset_password" , summary = "Сброс пароля" )
-async def reset_password (  ) :
-	pass
-
-@router.patch (  "/change_email" , summary = "Изменение электронной почты" )
-async def rename_email (  ) :
-	pass
-
-@router.patch (  "/change_avatar" , summary = "Изменение фотографии профиля" )
-async def rename_avatar (  ) :
+@router.patch ( "/reset_password" , summary = "Сброс пароля" )
+async def reset_password ( ) :
 	pass
 
 
-
-
-
-
-async def dispatch_registration_code ():
+@router.patch ( "/change_email" , summary = "Изменение электронной почты" )
+async def rename_email ( ) :
 	pass
 
 
-async def dispatch_registration_code2 ():
+@router.patch ( "/change_avatar" , summary = "Изменение фотографии профиля" )
+async def rename_avatar ( ) :
+	pass
+
+
+async def dispatch_registration_code ( ) :
+	pass
+
+
+async def dispatch_registration_code2 ( ) :
 	pass
--- Route/auth/response_model.py
+++ Route/auth/response_model.py
@@ -1,21 +1,67 @@
-from pydantic import BaseModel , validator , model_validator , field_validator
+from pydantic import BaseModel , validator , field_validator
 from pydantic_core.core_schema import FieldValidationInfo
+from sqlalchemy.orm import Session
+
+from Database.User_Table import User_Table
+
+db: Session = Session
 
 
-class RegistationResponseModel ( BaseModel ) :
+class CheckDataResponseModel ( BaseModel ) :
+	success: bool
+	message: str
+	
+	# проверка на успешность на крайний случай
+	@validator ( 'success' )
+	def validate_success ( cls , v ) :
+		if v == True | v == False :
+			return v
+		else :
+			raise ValueError (
+				'Не правильно проведена проверка'
+				)
+
+
+class CheckRegistationRequestModel ( BaseModel ) :
 	login: str
 	password: str
 	password2: str
 	email: str
 	
-	# заглушка на проверку email
+	# проверка логина
+	@validator ( 'login' )
+	def validate_email ( cls , value ) :
+		check_email = User_Table (
+			login_user = value ,
+			
+			)
+		
+		check = db.first ( check_email )
+		if check is None :
+			return value
+		else :
+			raise ValueError (
+				'Данный логин используется'
+				)
 	
+	# проверка email
 	@validator ( 'email' )
-	def validate_email (cls, value ) :
-		pass
+	def validate_email ( cls , value ) :
+		check_email = User_Table (
+			email = value ,
+			
+			)
+		
+		check = db.first ( check_email )
+		if check is None :
+			return value
+		else :
+			raise ValueError (
+				'Данная почта используется'
+				)
 	
 	@validator ( 'password' )
-	def validate_password (cls, value ) :
+	def validate_password ( cls , value ) :
 		if len ( value ) < 8 :
 			raise ValueError ( 'Пароль имеет меньше 8 символов' )
 		if not value :
@@ -28,12 +74,8 @@ class RegistationResponseModel ( BaseModel ) :
 				)
 		return value
 	
-	@field_validator ( 'password2')
-	def validate_password (cls, value, info: FieldValidationInfo) :
+	@field_validator ( 'password2' )
+	def validate_password ( cls , value , info: FieldValidationInfo ) :
 		if 'password' in info.data and value != info.data [ 'password' ] :
 			raise ValueError ( 'Введенные пароли не совпадают' )
 		return value
-	
-		
-	
-	
