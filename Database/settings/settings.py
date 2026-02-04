ae94a30f7659c0a5e5d500727a691fcef3132b56
jeived <jeived777@yandex.ru>
2026-02-04 17:47:44 +0300

    Подключен к проекту Docker и Docker Compose

--- Database/settings/settings.py
+++ Database/settings/settings.py
@@ -1,27 +1,23 @@
 import os
-from pydantic_settings import BaseSettings , SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    SECRET_KEY: str
 
+    
+    model_config = SettingsConfigDict(
+        env_file = (".env" ,) ,
+        env_file_encoding = 'utf-8' ,
+        extra = 'ignore'
+    )
 
+    def get_db_url(self):
+        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+                f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
 
-class Settings ( BaseSettings ) :
-	DB_USER: str
-	DB_PASSWORD: str
-	DB_HOST: str
-	DB_PORT: int
-	DB_NAME: str
-	SECRET_KEY: str
-	
-	
-	
-	model_config = SettingsConfigDict (
-		env_file = os.path.join ( os.path.dirname ( os.path.abspath ( __file__ ) ) , ".env" )
-		)
-	 
-	def get_db_url ( self ) :
-		return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
-		        f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
-	
-	
-
-settings = Settings ( )
+settings = Settings()
