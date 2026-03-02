b94004e2defa4ced09b9ded1b5b2bb3c49ac06a7
jeived <jeived777@yandex.ru>
2026-03-02 11:50:19 +0300

    Реализован сброс пароля

--- src/Database/settings/celery_app.py
+++ src/Database/settings/celery_app.py
@@ -49,8 +49,8 @@ celery_app.conf.update(
     worker_log_format="[%(asctime)s: %(levelname)s/%(processName)s] %(message)s",
     worker_task_log_format="[%(asctime)s: %(levelname)s/%(processName)s] [%(task_name)s(%(task_id)s)] %(message)s",
 
-    # Явно импортируем модули с задачами, чтобы воркер их зарегистрировал
-    imports=("celery_methods",),
+    # Явно импортируем модуль с задачами, чтобы воркер их зарегистрировал
+    imports=("app.workers.tasks",),
 
     # Чтобы HTTP-запросы не зависали при недоступном Redis
     broker_connection_timeout=5,
